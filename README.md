3649f2f0c42df51b2654f5193258ecb756ed180a
YeongKwonMo <momoci99@gmail.com>
2023-11-11 13:21:55 +0900

    init:project setup and add github package

---
+++ README.md
@@ -0,0 +1,3 @@
+# test-import-project
+
+- test-common-ui 컴포넌트를 사용하는 테스트 프로젝트 입니다.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { MyCounter } from "@momoci99/test-common-ui";
+
+function App() {
+  return (
+    <>
+      <MyCounter></MyCounter>
+    </>
+  );
+}
+
+export default App;
